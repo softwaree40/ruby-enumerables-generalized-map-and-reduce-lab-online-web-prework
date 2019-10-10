5701544be16abad66f6f89ffaf3d217716822b06
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-10 02:49:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,7 +6,18 @@ def map(array)
    return new_array
 end
 
-def reduce(array)
+def reduce(array,sv = nil)
+  if sv
+    num1 = sv
+    i=0 
+  else
+    num1 = array[0]
+    i = 1
+  end
+  while i < array.length 
+  num1 = yield(num1,array[i])
+  i+=1 
+end
    
     
 end
