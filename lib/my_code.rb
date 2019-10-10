7c7023b1bd12567b33f10bdfb9918ba8a199d393
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-10 02:50:24 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
-# Your Code Here
+def map(array)
+     new_array = []
+     array.each do |num|
+       new_array.push(yield(num)) 
+   end
+   return new_array
+end
+
+def reduce(array,sv = nil)
+  if sv
+    num1 = sv
+    i=0 
+  else
+    num1 = array[0]
+    i = 1
+  end
+  while i < array.length 
+  num1 = yield(num1,array[i])
+  i+=1 
+  end
+  num1
+end
