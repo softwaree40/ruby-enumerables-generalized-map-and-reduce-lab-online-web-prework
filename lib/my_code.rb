20df4bb37241319e6ab9132a487326fe4d9d8dfb
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-10 02:39:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
-# Your Code Here
+def map(array)
+     new_array = []
+     array.each do |num|
+       new_array.push(yield(num)) 
+   end
+   return new_array
+end
+
+def reduce(array)
+   
+    
+end
